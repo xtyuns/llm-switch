baf816f76a6ff4e75cac95b04feaf91ce0c298de
xtyuns <xtyuns@163.com>
2023-12-31 16:49:34 +0800

    handle error

--- src/main/kotlin/io/github/xtyuns/llmhub/core/Brand.kt
+++ src/main/kotlin/io/github/xtyuns/llmhub/core/Brand.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
+import org.springframework.web.client.RestClientResponseException
 import org.springframework.web.client.RestTemplate
 import java.util.zip.GZIPInputStream
 
@@ -34,21 +35,31 @@ interface Brand {
         data: String
     ): Triple<HttpStatusCode, HttpHeaders, String> {
         val restTemplate = RestTemplate()
-        val executed = restTemplate.execute(url, method, { request ->
-            headers.remove(HttpHeaders.CONTENT_LENGTH)
-            request.headers.addAll(headers)
-            val bys = data.toByteArray(Charsets.UTF_8)
-            request.body.write(bys)
-        }, { response ->
-            val responseHeaders = HttpHeaders()
-            response.headers.forEach { (key, value) ->
-                responseHeaders[key] = value
-            }
-            responseHeaders.remove(HttpHeaders.CONTENT_LENGTH)
-            val gzipInputStream = GZIPInputStream(response.body)
-            val responseData = String(gzipInputStream.readAllBytes(), Charsets.UTF_8)
-            Triple(response.statusCode, responseHeaders, responseData)
-        })
-        return executed!!
+        try {
+            val executed = restTemplate.execute(url, method, { request ->
+                request.headers.addAll(headers)
+                request.headers.remove(HttpHeaders.CONTENT_LENGTH)
+                request.headers.remove(HttpHeaders.HOST)
+                request.headers.remove(HttpHeaders.COOKIE)
+                val bys = data.toByteArray(Charsets.UTF_8)
+                request.body.write(bys)
+            }, { response ->
+                val responseHeaders = HttpHeaders()
+                response.headers.forEach { (key, value) ->
+                    responseHeaders[key] = value
+                }
+                responseHeaders.remove(HttpHeaders.CONTENT_LENGTH)
+                val gzipInputStream = GZIPInputStream(response.body)
+                val responseData = String(gzipInputStream.readAllBytes(), Charsets.UTF_8)
+                Triple(response.statusCode, responseHeaders, responseData)
+            })
+            return executed!!
+        } catch (e: RestClientResponseException) {
+            e.printStackTrace()
+            return Triple(e.statusCode, e.responseHeaders ?: HttpHeaders.EMPTY, e.responseBodyAsString)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return Triple(HttpStatus.INTERNAL_SERVER_ERROR, HttpHeaders.EMPTY, "")
+        }
     }
 }
